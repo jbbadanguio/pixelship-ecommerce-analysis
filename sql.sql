9d5b4540478d01d8c2e0ef0f07bb34091bdd5ed1
jbbadanguio <j.badanguio@gmail.com>
2025-10-08 20:46:14 -0700

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -1,8 +1,8 @@
 /*
-Pixelship: Supply Chain & Profitability Analysis
+PixelShip: Supply Chain & Profitability Analysis
 
 Objective: 
-To analyze Pixelship's global order fulfillment performance from 2019-2022, identify the key drivers of shipping delays, and quantify their direct impact on customer refunds and company profitability.
+To analyze PixelShip's global order fulfillment performance from 2019-2022, identify the key drivers of shipping delays, and quantify their direct impact on customer refunds and company profitability.
 */
 
 
